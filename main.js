f88de3ddfc5963d6f288d5acb49970f6d5d92603
ullah <sheikholeei97@gmail.ocm>
2022-08-14 16:36:25 +0600

    3. valid input color code feature added.

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
+// global variable calling for many purpose as demand as my projcet
 let div = null;
 let btn,input,copyBtn;
+
+
 // main function calling
 window.onload = function () {
     main();
     unlock();
 }
-
-
 
 
 // Main function defination.
@@ -26,15 +27,19 @@ function main() {
 
 // eventlisten 2
     copyBtn.addEventListener('click', function () {
-        navigator.clipboard.writeText(input.value);
-
         if(div != null){
             div.remove();
             div = null;
         }
 
+        if(isValidHex(input.value)){
+            navigator.clipboard.writeText(input.value);
+            toastMessage(input.value);
+        } else {
+            alert('Can\'t copy a invalid code')
+        }
+
         // calling the toast messagefunction
-        toastMessage(input.value);
 
 
     })
@@ -43,19 +48,13 @@ function main() {
 
     input.addEventListener('keyup', function (event) {
         const colorcode = event.target.value;
-        root.style.backgroundColor = colorcode
+        if(isValidHex(colorcode)) {
+            root.style.backgroundColor = colorcode;
+        }
     })
-
 }
 
-// changecolor function rgb type
-// function changeColor() {
-//     const red = Math.floor(Math.random() * 255);
-//     const green = Math.floor(Math.random() * 255);
-//     const blue = Math.floor(Math.random() * 255);
-//     const rgb = `rgb(${red}, ${green}, ${blue})`;
-//     return rgb;
-// }
+
 
 // making rgb color to hexcolor
 function changeColor() {
@@ -68,7 +67,6 @@ function changeColor() {
 
 
 // toast message function
-
 function toastMessage(msg) {
     div = document.createElement('div');
     const p = document.createElement('p')
@@ -84,26 +82,33 @@ function toastMessage(msg) {
         div.addEventListener('animationend', function () {
             div.remove();
         })
-
     })
     } else {
         alert('Nothing to copy !!!')
     }
-
 }
 
 
-// unlocking the copy button
 
+// unlocking the copy button
 function unlock() {
     const unlockBtn = document.getElementById('uninput');
     unlockBtn.addEventListener('keyup', function (event) {
-        console.log(event.target.value);
         if((event.target.value).toLowerCase() == 'unlock'){
-            console.log(btn);
             btn.removeAttribute('disabled');
             copyBtn.removeAttribute('disabled');
             input.removeAttribute('disabled');
         }
     })
 }
+
+
+
+//  cheking the valid color code functio
+function isValidHex(code) {
+    if(code.length != 7) return false;
+    if(code[0] != '#') return false;
+    code = code.substring(1);
+    return /^[0-9A-Fa-f]{6}$/i.test(code);
+}
+
